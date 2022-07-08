a8e1070d5cab79255e43590b0f8ffacc35d4ec12
LeeChangHwan <changhwanlee94@gmail.com>
2022-07-08 17:12:32 +0900

    learn-redux-thunk: redux-thunk

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -7,11 +7,14 @@ import { applyMiddleware, legacy_createStore } from 'redux';
 import rootReducer from './modules';
 import logger from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import ReduxThunk from 'redux-thunk';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const store = legacy_createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(logger)) // 여러개의 미들웨어를 적용할 수 있다.
+
+  // logger 를 사용 할 경우, logger가 가장 마지막에 와야 한다.
+  composeWithDevTools(applyMiddleware(ReduxThunk, logger)) // 여러개의 미들웨어를 적용할 수 있다.
 );
 root.render(
   <React.StrictMode>
