449e5f502aab6f23163088c8fd4dca092c2788ea
LeeChangHwan <changhwanlee94@gmail.com>
2022-07-08 16:20:42 +0900

    learn-redux-middleware: 미들웨어 만들어보기

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app';
 import { Provider } from 'react-redux';
-import { legacy_createStore } from 'redux';
+import { applyMiddleware, legacy_createStore } from 'redux';
 import rootReducer from './modules';
+import myLogger from './middlewares/myLogger';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const store = legacy_createStore(rootReducer);
+const store = legacy_createStore(rootReducer, applyMiddleware(myLogger));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
